2be440f35955bd4f2f8e18cc83321f7d15fd62d1
viper15 <smuaz@yahoo.com>
2012-04-23 12:48:26 +0800

    modifed to config assets

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,8 @@ module SampleApp
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    config.assets.precompile += %w( *.js *.css )
+    
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
